6993075b9796fb1ea56f34b53c7b632bd24731f4
nisha <nishadmuhammed893@gmail.com>
2024-08-26 12:01:05 +0530

    gg

---
+++ lib/ui/homepage.dart
@@ -0,0 +1,74 @@
+
+import 'package:apigetexam/ui/producttile.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/instance_manager.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+
+
+import '../controller/controller.dart';
+
+class HomePage extends StatelessWidget {
+  final ProductController  productController = Get.put(ProductController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue,
+        elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Shopify',
+                    style: TextStyle(
+                        fontFamily: 'avenir',
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(Icons.view_list_rounded), onPressed: () {}),
+                IconButton(icon: Icon(Icons.grid_view), onPressed: () {}),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Obx(() {
+              if (productController.isLoading.value)
+                return Center(child: CircularProgressIndicator());
+              else
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 2,
+                  itemCount: productController.productList.length,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  itemBuilder: (context, index) {
+                    return ProductTile(productController.productList[index]);
+                  },
+                  staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                );
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
